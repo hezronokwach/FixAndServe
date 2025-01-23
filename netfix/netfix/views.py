f386ca517e2f56cb176b4345379474713945f4b6
hezronokwach <hezronokwach@gmail.com>
2025-01-23 14:43:50 +0300

    fix[profile]: Make the profile dynamic to the user who signed in either a customer or company

--- netfix/netfix/views.py
+++ netfix/netfix/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from datetime import date
 
 from users.models import User, Company
 from services.models import Service
@@ -8,8 +9,22 @@ def home(request):
     return render(request, 'users/home.html', {'user': request.user})
 
 
-def customer_profile(request):
-    pass
+def customer_profile(request, name):
+    # fetches the customer user
+    user = User.objects.get(username=name)
+    customer = user.customer
+    
+    # Calculate age from birth date
+    today = date.today()
+    birth_date = customer.birth
+    user_age = None
+    if birth_date:
+        user_age = today.year - birth_date.year - ((today.month, today.day) < (birth_date.month, birth_date.day))
+    
+    return render(request, 'users/profile.html', {
+        'user': user,
+        'user_age': user_age
+    })
 
 
 def company_profile(request, name):
@@ -18,4 +33,7 @@ def company_profile(request, name):
     services = Service.objects.filter(
         company=Company.objects.get(user=user)).order_by("-date")
 
-    return render(request, 'users/profile.html', {'user': user, 'services': services})
+    return render(request, 'users/profile.html', 
+    {'user': user, 
+    'services': services
+    })
